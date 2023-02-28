6050a6408972b5e67f0d960edd3e4ce77502835e
Hetal Solanki <Hetal.Solanki@einfochips.com>
2023-02-28 12:32:14 +0530

    Added assignments

--- Day8/demo1/demo2.py
+++ Day8/demo1/demo2.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-driver= webdriver.Firefox()
+driver= webdriver.Chrome()
 driver.maximize_window() # For maximizing window
 driver.implicitly_wait(20)
 driver.get("https://www.automationworld.com/")
@@ -12,21 +12,26 @@ driver.implicitly_wait(5)
 driver.find_element(By.XPATH,"//div[contains(@class,'close')]").click()
 driver.find_element(By.LINK_TEXT,"Subscribe").click()
 #driver.implicitly_wait(10)
+driver.switch_to.window(driver.window_handles[1])
+print(driver.title)
+time.sleep(20)
 #driver.find_element(By.XPATH,"//input[@name='demo59701']]").click()
-time.sleep(20)
 driver.find_element(By.XPATH,"//input[@name='demo59703']").send_keys('John')
-time.sleep(20)
-#driver.find_element(By.NAME,'demo59704').send_keys('wign')
-#driver.find_element(By.NAME,'demo59705').send_keys('QA')
-#driver.find_element(By.NAME,'demo59707').send_keys('www.Arrow.com')
-#driver.find_element(By.NAME,'demo59708').send_keys('eInfochips')
-#driver.find_element(By.XPATH,"//option[text()='INDIA']").click()
+driver.find_element(By.NAME,'demo59704').send_keys('wign')
+driver.find_element(By.NAME,'demo59705').send_keys('QA')
+driver.find_element(By.NAME,'demo59707').send_keys('www.Arrow.com')
+driver.find_element(By.NAME,'demo59708').send_keys('eInfochips')
+driver.find_element(By.XPATH,"//option[text()='INDIA']").click()
 #driver.find_element(By.NAME,'demo59713').send_keys('Chennai')
-#driver.find_element(By.NAME,'demo59693').click()
-#driver.find_element(By.XPATH,"//input[@type='submit']").click()
 
-#driver.implicitly_wait(5)
-actual_error=driver.find_element(By.LINK_TEXT,'Email Address is required').text
+#Javascript
+driver.execute_script("document.querySelector('input[name=\"demo59713\"]').value='Chennai'") # before the double quotes we need to add slash
+
+driver.find_element(By.NAME,'demo59693').click()
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
+
+driver.implicitly_wait(5)
+actual_error=driver.find_element(By.XPATH,"//li[contains(text(),'Email Address is required.')]").text
 print(actual_error)
 
 time.sleep(20)
